4ff61e1515a1a0e9ea488a5644a1c7700896adcc
Interzoneism <martinforslund.studio@gmail.com>
2025-09-06 01:35:52 +0200

    Added new reference code for IH

---
+++ Working_Fetch_Code/ModSys.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+
+namespace ImprovedHandbookRecipes;
+public class ModSys : ModSystem {
+    private const string ID = "improvedhandbookrecipes";
+
+    private Harmony harmony;
+
+    public override void StartPre(ICoreAPI api) 
+        => (harmony ??= new Harmony(ID)).PatchAll();
+
+    public override void Dispose() 
+        => harmony?.UnpatchAll(ID);
+
+    public override void StartClientSide(ICoreClientAPI api) {
+        Handbook_Patch.SetAPI(api);
+
+        Textures.Load(api);
+    }
+
+}
