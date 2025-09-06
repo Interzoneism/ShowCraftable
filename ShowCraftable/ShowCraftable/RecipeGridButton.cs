a4e5ad9d53d71a9256e2559c0bf1d26929cce8f9
Interzoneism <113020222+Interzoneism@users.noreply.github.com>
2025-09-06 22:00:39 +0200

    feat: add handbook recipe button

--- ShowCraftable/ShowCraftable/RecipeGridButton.cs
+++ ShowCraftable/ShowCraftable/RecipeGridButton.cs
@@ -4,52 +4,26 @@ namespace ShowCraftable;
 
 public class RecipeGridButton : ButtonRTC
 {
-    public RecipeGridButton(ICoreClientAPI api) : base(api, 0, "#", "Fetch all the ingredients", -1.0, -120)
+    private const double YManualOffset = -10.0;
+
+    public RecipeGridButton(ICoreClientAPI api)
+        : base(api, 0, "#", "Fetch all the ingredients", -1.0, -10.5)
     {
         Float = EnumFloat.Inline;
         VerticalAlign = EnumVerticalAlign.FixedOffset;
     }
 
-    // Gör inget extra – knappelementet spelar sitt eget klickljud (PlaySound = true).
-    protected override void OnClick() 
+    protected override void OnClick()
     {
         api.Gui.PlaySound("menubutton_press");
     }
 
-    private struct Bounds
+    public override EnumCalcBoundsResult CalcBounds(TextFlowPath[] flowPath, double currentLineHeight, double offsetX, double lineY, out double nextOffsetX)
     {
-        private int xPos;
-        private int yPos;
-        private readonly int xLen;
-        private readonly int yLen;
-        private readonly int outerLen;
-
-        public Bounds(int xLen, int yLen, int outerLen)
-        {
-            this.xLen = xLen;
-            this.yLen = yLen;
-            this.outerLen = outerLen;
-        }
-
-        public void Align(int outer, int inner)
-        {
-            int x = outer % outerLen - inner % xLen;
-            int y = outer / outerLen - inner / xLen;
-            if (x >= 0 && y >= 0 && x + xLen < outerLen && y + yLen < outerLen)
-            {
-                xPos = x;
-                yPos = y;
-            }
-        }
-
-        public readonly int ToInner(int outer)
-            => outer % outerLen - xPos + (outer / outerLen - yPos) * xLen;
-
-        public readonly bool Contains(int i)
-        {
-            int x = i % outerLen, y = i / outerLen;
-            return x >= xPos && x < xPos + xLen
-                && y >= yPos && y < yPos + yLen;
-        }
+        var result = base.CalcBounds(flowPath, currentLineHeight, offsetX, lineY, out nextOffsetX);
+        var r = BoundsPerLine[0];
+        r.Y += GuiElement.scaled(YManualOffset);
+        BoundsPerLine[0] = r;
+        return result;
     }
 }
